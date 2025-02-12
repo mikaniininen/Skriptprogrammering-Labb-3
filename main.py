af436203f962f49ea0d9b0f6392e33dbbb38888e
Mika <h24mniin@du.se>
2025-02-12 16:40:28 +0100

    mer saker

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-import requests, json
+import requests, json, methods
+
+def main():
+    user_choice = int(input())
+    match user_choice:
+        case 1:
+            methods.search_title()
+        case 2:
+            print('nej')
+        case 3:
+            methods.search_word()
+        case _:
+            print('ERROR')
+
+main()
+
+
+
 
--- methods.py
+++ methods.py
@@ -1,28 +1,86 @@
 import requests, json
 
 
-
 api_key = 'cc26ded9'
-
+json_file = 'file_str.json'
 
 
 def search_title():
     user_title = input('Skriv din titel: ')
     url = 'https://omdbapi.com/?apikey=' + api_key + '&t=' + user_title
     fetch_url_json(url)
-    #search_history.append(user_title)
+    add_history(user_title)
+
+def add_history(user_title):
     with open('search_json.json', 'w', encoding='utf-8') as fpointer:
         wdata = json.dumps(user_title)
-        fpointer.write(user_title)
+        fpointer.write(wdata)
+    print_json(json_file)
 
 def fetch_url_json(url):
     response = requests.get(url)
     data = response.json()
+    if data.get("Response") == "False":
+        print("Din sökning fick inga resultat.")
+
     with open('file_str.json', 'w', encoding='utf-8') as fpointer:
-        wdata = json.dumps(data)
+        wdata = json.dumps(data, indent=4)
         fpointer.write(wdata)
+
+   # if response.status_code != 200:
+    #    print('Anslutningen misslyckades')
+
+
+
+def print_json(json_file):
+    try:
+        with open(json_file, 'r', encoding = 'utf-8-sig') as jsonf:
+            info_dict = json.load(jsonf)
+
+        keys_to_extract = ['Title', 'Year', 'Runtime', 'Genre', 'Director', 'Actors']
+        filtered_data = {key:info_dict[key] for key in keys_to_extract if key in info_dict}
+
+        with open(json_file, 'w', encoding='utf-8-sig') as jsonf:
+            json.dump(filtered_data, jsonf)
+            output = json.dumps(filtered_data, indent=4, ensure_ascii=False)
+        print(output)
+
+
+    except FileNotFoundError:
+        print('\nERROR: Filen finns inte\n')
+
+
+#def print_history():
+
+
+
+def search_word():
+    user_word = input('Skriv ditt sökord: ')
+    url = 'https://omdbapi.com/?apikey=' + api_key + '&s=' + user_word
+    fetch_url_json(url)
+    with open(json_file, 'r', encoding='utf-8') as jsonf:
+        results = json.load(jsonf)
+
+    if "Search" not in results:
+        print("Inga resultat.")
+        return
+
+    keys_to_extract = ['Title', 'Year']
+    filtered_data = [{key: movie[key] for key in keys_to_extract if key in movie} for movie in results['Search']]
+
+    with open(json_file, 'w', encoding='utf-8-sig') as jsonf:
+        json.dump(filtered_data, jsonf)
+    output = json.dumps(filtered_data, indent=4, ensure_ascii=False)
+    print(output)
+
+
+
+
+
+
+
 
 
 #fetch_url_json(url)
 
-search_title()
+
